9867827ae33dcac3c0cd00eb075bd008f1bf22c9
acsbendi <acsbendi28@gmail.com>
2019-01-18 12:49:12 +0100

    Fixed rebuild.bat only working in certain circumstances

--- rebuild.bat
+++ rebuild.bat
@@ -1,6 +1,6 @@
-copy /y service\service.go %GOPATH%\src\github.com\NYTimes\mock-ec2-metadata\service\service.go
+make deps
+echo F | xcopy /y service\service.go %GOPATH%\src\github.com\NYTimes\mock-ec2-metadata\service\service.go
 del bin\m2.exe
 copy /y mock-ec2-metadata-config.json bin\mock-ec2-metadata-config.json
 %GOPATH%/bin/gox -output "bin/m2" -os="windows" ./
-cd bin 
-m2
+bin\m2
